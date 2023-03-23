fa938b9729d33db3464ed4cf7451c8e94f0363a8
zhumengdi <jurnee.zhu@ucloud.cn>
2023-03-23 17:28:44 +0800

    export excel

--- time/time_convert_test.go
+++ time/time_convert_test.go
@@ -2,9 +2,9 @@ package time
 
 import (
 	"testing"
-	"tool/goutils/common"
+	"tool/utils/common"
 )
 
 func Test_TimeConvTimestemp(t *testing.T) {
-	t.Log(TimeConvTimestemp(common.Time_Format_1, "2021-10-01 00:00:00"))
+	t.Log(TimeConvTimestemp(common.Time_Format_Second, "2021-10-01 00:00:00"))
 }
