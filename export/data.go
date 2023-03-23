b85976beeb95426aa200be33e42d92d835bc74d5
zhumengdi <jurnee.zhu@ucloud.cn>
2023-03-23 15:01:46 +0800

    export

---
+++ export/data.go
@@ -0,0 +1,10 @@
+package export
+
+// type Export struct {
+// 	headName []string
+// 	data     []*SignalEx
+// }
+
+// type SignalEx struct {
+// 	s []interface{}
+// }
--- export/excel.go
+++ export/excel.go
@@ -1,4 +1,4 @@
-package excel
+package export
 
 import (
 	"errors"
