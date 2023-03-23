6261ed19b6d72a27021fa15ef4e802f4b57703cc
zhumengdi <jurnee.zhu@ucloud.cn>
2023-03-23 17:30:01 +0800

    Time_Format_Million_Second

--- common/const.go
+++ common/const.go
@@ -1,4 +1,7 @@
 package common
 
 // time format
-var Time_Format_Second = "2006-01-02 15:04:05"
+const (
+	Time_Format_Second         = "2006-01-02 15:04:05"
+	Time_Format_Million_Second = "2006-01-02 15:04:05.000"
+)
