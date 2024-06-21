94e173fcac3a39fa5721a181490f015ee4abcfde
Jurneez <medizh09@163.com>
2024-06-21 16:15:39 +0800

    buildBigHeap

--- IntSliceSort/sort.go
+++ IntSliceSort/sort.go
@@ -147,7 +147,7 @@ func HeapSort(numbers []int64) {
 	length := len(numbers)
 	for i := 0; i < length; i++ {
 		fmt.Println("待排序的初始值", numbers[:length-i])
-		singleHeapSort(numbers[:length-i], 0)
+		buildBigHeap(numbers[:length-i], 0)
 		fmt.Println("堆排序结果", numbers)
 		numbers[0], numbers[length-i-1] = numbers[length-i-1], numbers[0]
 		fmt.Println("排序结果", numbers)
@@ -160,7 +160,7 @@ len = 数组长度
 叶子节点： i <= len/2+1
 非叶子节点：i > len/2 +1
 */
-func singleHeapSort(numbers []int64, index int64) {
+func buildBigHeap(numbers []int64, index int64) {
 	if index > int64(len(numbers)-1) {
 		return
 	}
@@ -168,8 +168,8 @@ func singleHeapSort(numbers []int64, index int64) {
 	if index > int64(temp) { // 叶子节点，不做处理
 		return
 	} else { // 非叶子节点
-		singleHeapSort(numbers, 2*index+1) // 处理左节点
-		singleHeapSort(numbers, 2*index+2) // 处理右节点
+		buildBigHeap(numbers, 2*index+1) // 处理左节点
+		buildBigHeap(numbers, 2*index+2) // 处理右节点
 		if (2*index+1) <= int64(len(numbers)-1) && numbers[index] < numbers[2*index+1] {
 			numbers[index], numbers[2*index+1] = numbers[2*index+1], numbers[index]
 		}
